4b9a3be6875a5eb53f046a3a4bdcd51b99633e48
Alejandro Rosas <alejandrorosasdev@gmail.com>
2015-02-03 14:14:24 +0100

    Port selection working

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QtCore/QDebug>
+#include <QScrollBar>
 
 #include <QtSerialPort/QSerialPortInfo>
 #include <QtSerialPort/QSerialPort>
 QT_USE_NAMESPACE
 
-QSerialPortInfo port;
+QSerialPortInfo port[100];
 
 #define BAUD_RATE 9600
+
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -19,9 +22,10 @@ MainWindow::MainWindow(QWidget *parent) :
     serial = new QSerialPort(this);
 
     ui->comboBox->clear();
+    int i = 0;
     foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
            ui->comboBox->addItem(""+ info.portName());
-           port = info;
+           port[i++] = info;
     }
     //Test GitHub
     connect(serial, SIGNAL(readyRead()), this, SLOT(readData()));
@@ -36,7 +40,7 @@ void MainWindow::portConnect()
 {
     if(ui->butConnect->text().startsWith("Connect"))
     {
-        serial->setPort(port);
+        serial->setPort(port[ui->comboBox->currentIndex()]);
         serial->open(QIODevice::ReadWrite);
         serial->setBaudRate(BAUD_RATE);
         ui->butConnect->setText("Close");
@@ -53,4 +57,5 @@ void MainWindow::readData()
     QByteArray data = serial->readAll();
     QString old_data = ui->textEdit->toPlainText();
     ui->textEdit->setText(old_data+data);
+    ui->textEdit->verticalScrollBar()->setValue(ui->textEdit->verticalScrollBar()->maximum());
 }
